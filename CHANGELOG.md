706863dd7a007270a5ffbf8acc253b405be6c659
CrazyMax <crazy-max@users.noreply.github.com>
2019-06-28 01:28:51 +0200

    Initial version based on Wire 3.9.2928

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 3.9.2928-1 (2019/06/28)
+
+* Initial version based on Wire 3.9.2928
---
+++ main.go
@@ -0,0 +1,33 @@
+//go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+//go:generate goversioninfo -icon=res/papp.ico
+package main
+
+import (
+	"os"
+
+	. "github.com/portapps/portapps"
+	"github.com/portapps/portapps/pkg/utl"
+)
+
+var (
+	app *App
+)
+
+func init() {
+	var err error
+
+	// Init app
+	if app, err = New("wire-portable", "Wire"); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
+}
+
+func main() {
+	utl.CreateFolder(app.DataPath)
+	electronBinPath := utl.PathJoin(app.AppPath, utl.FindElectronAppFolder("app-", app.AppPath))
+
+	app.Process = utl.PathJoin(electronBinPath, "Wire.exe")
+	app.WorkingDir = electronBinPath
+
+	app.Launch(os.Args[1:])
+}
