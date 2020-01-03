1f21f6dc7d003595c68db5946c4a97d176878db7
CrazyMax <crazy-max@users.noreply.github.com>
2020-01-03 11:39:35 +0100

    Add cleanup config

--- main.go
+++ main.go
@@ -4,20 +4,31 @@ package main
 
 import (
 	"os"
+	"path"
 
 	. "github.com/portapps/portapps"
 	"github.com/portapps/portapps/pkg/utl"
 )
 
+type config struct {
+	Cleanup bool `yaml:"cleanup" mapstructure:"cleanup"`
+}
+
 var (
 	app *App
+	cfg *config
 )
 
 func init() {
 	var err error
 
+	// Default config
+	cfg = &config{
+		Cleanup: false,
+	}
+
 	// Init app
-	if app, err = New("wire-portable", "Wire"); err != nil {
+	if app, err = NewWithCfg("wire-portable", "Wire", cfg); err != nil {
 		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
@@ -28,6 +39,15 @@ func main() {
 
 	app.Process = utl.PathJoin(electronBinPath, "Wire.exe")
 	app.WorkingDir = electronBinPath
+
+	// Cleanup on exit
+	if cfg.Cleanup {
+		defer func() {
+			utl.Cleanup([]string{
+				path.Join(os.Getenv("APPDATA"), "Wire"),
+			})
+		}()
+	}
 
 	app.Launch(os.Args[1:])
 }
